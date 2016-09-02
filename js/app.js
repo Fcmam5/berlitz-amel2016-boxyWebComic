72fbdb5c0d66f1d1127fe6a5cf7b2c9bbd3330d5
fcmam5 <dr.fcmam5@gmail.com>
2016-09-02 16:26:21 +0100

    workon angular, baby step

--- js/app.js
+++ js/app.js
@@ -14,15 +14,41 @@ webComic.config(function ($routeProvider) {
         .when('/about', {
             templateUrl: 'partials/about.html'
         })
+        .when('/read', {
+            templateUrl: 'partials/read.html'
+        })
         .otherwise({redirectTo: '/'})
 });
 var controllers = {};
-controllers.chapterList = function($scope){
+controllers.chapterList = function ($scope) {
     /**/
     $scope.chapters = [
-        {'name': "Chapter01", 'link': "#"},
-        {'name': "Chapter02", 'link': "#"},
-        {'name': "Chapter03", 'link': "#"}
+        {
+            'num': 1,
+            'name': "Chapter01",
+            'title': 'First day bla bla',
+            'link': "#",
+            'comicImg': 'http://th01.deviantart.net/fs70/PRE/i/2013/126/1/e/nature_portrait_by_pw_fotografie-d63tx0n.jpg',
+            'story': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda at dignissimos ex maxime quo voluptatibus? Assumenda autem commodi dolore eligendi molestias obcaecati, officiis possimus quia reprehenderit temporibus ullam vel',
+            'shortStory': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+        },
+        {
+            'num': 2,
+            'name': "Chapter02",
+            'title': 'First day bla bla',
+            'link': "#",
+            'comicImg': 'http://th01.deviantart.net/fs70/PRE/i/2013/126/1/e/nature_portrait_by_pw_fotografie-d63tx0n.jpg',
+            'story': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda at dignissimos ex maxime quo voluptatibus? Assumenda autem commodi dolore eligendi molestias obcaecati, officiis possimus quia reprehenderit temporibus ullam vel',
+            'shortStory': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'        },
+        {
+            'num': 3,
+            'name': "Chapter03",
+            'title': 'First day bla bla',
+            'link': "#",
+            'comicImg': 'http://th01.deviantart.net/fs70/PRE/i/2013/126/1/e/nature_portrait_by_pw_fotografie-d63tx0n.jpg',
+            'story': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda at dignissimos ex maxime quo voluptatibus? Assumenda autem commodi dolore eligendi molestias obcaecati, officiis possimus quia reprehenderit temporibus ullam vel',
+            'shortStory': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+        }
     ];
 };
 
--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@
  */
 $(document).ready(function () {
     $(".button-collapse").sideNav();
+    $('select').material_select();
 });
