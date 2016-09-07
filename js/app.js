330a954313342f55da77fad28596b85ac1e47255
fcmam5 <dr.fcmam5@gmail.com>
2016-09-07 03:09:58 +0100

    add first chapter images

--- js/app.js
+++ js/app.js
@@ -26,40 +26,73 @@ controllers.chapterList = function ($scope) {
     /*todo: A chapter contains many images*/
     $scope.chapters = [
         {
-            'num': 0,
-            'name': "Chapter00",
-            'title': 'Zero day bla bla',
-            'link': "#",
-            'comicImg': 'http://th01.deviantart.net/fs70/PRE/i/2013/126/1/e/nature_portrait_by_pw_fotografie-d63tx0n.jpg',
+            'num': 1,
+            'name': "Chapter01",
+            'title': 'Boxy\'s 2016 summer',
+            'comicImgs': [
+                'http://i.imgur.com/HUat3x3.jpg',
+                'http://i.imgur.com/mewb23D.jpg',
+                'http://i.imgur.com/GGYQyuv.jpg',
+                'http://i.imgur.com/konAR3v.jpg',
+                'http://i.imgur.com/pxdnhyk.jpg',
+                'http://i.imgur.com/4Thv6bZ.jpg',
+                'http://i.imgur.com/TrUDpeR.jpg',
+                'http://i.imgur.com/a56N1k8.jpg',
+                'http://i.imgur.com/fT4NmNw.jpg',
+                'http://i.imgur.com/c9l9y3n.jpg',
+                'http://i.imgur.com/jSIV5ej.jpg',
+                'http://i.imgur.com/SsidnfG.jpg',
+                'http://i.imgur.com/l2H0Xl8.jpg',
+                'http://i.imgur.com/r8b3KPj.jpg',
+                'http://i.imgur.com/Ze6Z5QZ.jpg',
+                'http://i.imgur.com/dWskboB.jpg',
+                'http://i.imgur.com/DFg52Ly.jpg',
+                'http://i.imgur.com/Y4EVKzO.jpg',
+                'http://i.imgur.com/svjLVEs.jpg',
+                'http://i.imgur.com/JSRrCY3.jpg',
+                'http://i.imgur.com/HBEVxso.jpg',
+                'http://i.imgur.com/ipFIpXo.jpg',
+                'http://i.imgur.com/WDrygil.jpg',
+                'http://i.imgur.com/snjsnus.jpg'
+            ],
+            /*todo: change to real text*/
             'story': 'Imma zero orem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda at dignissimos ex maxime quo voluptatibus? Assumenda autem commodi dolore eligendi molestias obcaecati, officiis possimus quia reprehenderit temporibus ullam vel',
             'shortStory': 'Zeroes Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
         },
+        /*for testing*/
         {
             'num': 1,
             'name': "Chapter01",
             'title': 'First day bla bla',
             'link': "#",
-            'comicImg': 'http://th01.deviantart.net/fs70/PRE/i/2013/126/1/e/nature_portrait_by_pw_fotografie-d63tx0n.jpg',
+            'comicImgs': [
+                'http://i.imgur.com/HUat3x3.jpg',
+                'http://i.imgur.com/mewb23D.jpg',
+                'http://i.imgur.com/GGYQyuv.jpg',
+                'http://i.imgur.com/konAR3v.jpg',
+                'http://i.imgur.com/pxdnhyk.jpg',
+                'http://i.imgur.com/4Thv6bZ.jpg',
+                'http://i.imgur.com/TrUDpeR.jpg',
+                'http://i.imgur.com/a56N1k8.jpg',
+                'http://i.imgur.com/fT4NmNw.jpg',
+                'http://i.imgur.com/c9l9y3n.jpg',
+                'http://i.imgur.com/jSIV5ej.jpg',
+                'http://i.imgur.com/SsidnfG.jpg',
+                'http://i.imgur.com/l2H0Xl8.jpg',
+                'http://i.imgur.com/r8b3KPj.jpg',
+                'http://i.imgur.com/Ze6Z5QZ.jpg',
+                'http://i.imgur.com/dWskboB.jpg',
+                'http://i.imgur.com/DFg52Ly.jpg',
+                'http://i.imgur.com/Y4EVKzO.jpg',
+                'http://i.imgur.com/svjLVEs.jpg',
+                'http://i.imgur.com/JSRrCY3.jpg',
+                'http://i.imgur.com/HBEVxso.jpg',
+                'http://i.imgur.com/ipFIpXo.jpg',
+                'http://i.imgur.com/WDrygil.jpg',
+                'http://i.imgur.com/snjsnus.jpg'
+            ],
             'story': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda at dignissimos ex maxime quo voluptatibus? Assumenda autem commodi dolore eligendi molestias obcaecati, officiis possimus quia reprehenderit temporibus ullam vel',
             'shortStory': ' 01 Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
-        },
-        {
-            'num': 2,
-            'name': "Chapter02",
-            'title': 'Second day bla bla',
-            'link': "#",
-            'comicImg': 'http://th01.deviantart.net/fs70/PRE/i/2013/126/1/e/nature_portrait_by_pw_fotografie-d63tx0n.jpg',
-            'story': '22 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda at dignissimos ex maxime quo voluptatibus? Assumenda autem commodi dolore eligendi molestias obcaecati, officiis possimus quia reprehenderit temporibus ullam vel',
-            'shortStory': '2 Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
-        },
-        {
-            'num': 3,
-            'name': "Chapter03",
-            'title': 'Third day bla bla',
-            'link': "#",
-            'comicImg': 'http://th01.deviantart.net/fs70/PRE/i/2013/126/1/e/nature_portrait_by_pw_fotografie-d63tx0n.jpg',
-            'story': ' 33 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda at dignissimos ex maxime quo voluptatibus? Assumenda autem commodi dolore eligendi molestias obcaecati, officiis possimus quia reprehenderit temporibus ullam vel',
-            'shortStory': ' 3Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
         }
     ];
     $scope.chapter = function (id) {
